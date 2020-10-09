60ac8af9284053bfeae5535a4f2633b6fd2ffe91
Helvin <hjwc86@gmail.com>
2020-10-09 14:47:25 +0200

    correction conlfits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AuthComponent} from './auth/auth.component';
 import {TechComponent} from './tech/tech.component';
-
 import {AuthInterceptorService} from './auth/auth-interceptor.service';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import { SaisieNoteDeFraisComponent } from './saisie-note-de-frais/saisie-note-de-frais.component';
 import { SaisieNatureComponent } from './saisie-nature/saisie-nature.component';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AjoutNatureComponent } from './saisie-nature/ajout-nature/ajout-nature.component';
+import { ModifierNatureComponent } from './saisie-nature/modifier-nature/modifier-nature.component';
+import { SupprimerNatureComponent } from './saisie-nature/supprimer-nature/supprimer-nature.component';
 
 
 @NgModule({

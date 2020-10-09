f8efbda3371739e94280762d77d6b1ab746f3a31
Helvin <hjwc86@gmail.com>
2020-10-09 15:26:45 +0200

    modifif iste deroulante #8

--- src/app/pages/mission/demade-mission/demade-mission.component.ts
+++ src/app/pages/mission/demade-mission/demade-mission.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Mission } from 'src/app/models/mission';
+import { Nature } from 'src/app/models/nature';
+import { NatureService } from 'src/app/services/nature.service';
 
 @Component({
   selector: 'app-demade-mission',
@@ -9,12 +11,16 @@ import { Mission } from 'src/app/models/mission';
 })
 export class DemadeMissionComponent implements OnInit {
 
-  mission:Mission = new Mission(1, new Date(1995, 11, 17) ,new Date(1995, 11, 17)  ,'conseil','caracas','montpellier','bus','valide',10)
+  mission: Mission = new Mission(1, new Date(1995, 11, 17), new Date(1995, 11, 17), 'conseil', 'caracas', 'montpellier', 'bus', 'valide', 10)
+  listNature: Nature[]
 
-
-  constructor(public activeModal: NgbActiveModal) { }
+  constructor(public activeModal: NgbActiveModal, public natureService: NatureService) { }
 
   ngOnInit(): void {
+    this.natureService.listeNatures().subscribe(
+      listN => this.listNature = listN
+    )
+
   }
 
 }

f8f2efde527ead618c24203dccf27d56e54fc1e3
Helvin <hjwc86@gmail.com>
2020-10-16 10:59:55 +0200

    fenetre primesavec graphique

--- src/app/pages/primes/primes.component.ts
+++ src/app/pages/primes/primes.component.ts
@@ -1,45 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx';
-
+import * as Highcharts from 'highcharts';
 import { Collegue } from 'src/app/auth/auth.domains';
 import { AuthService } from 'src/app/auth/auth.service';
 import { Mission } from 'src/app/models/mission';
 import { MissionService } from 'src/app/services/mission.service';
-
-export class graph {
-  constructor(public annee: any, public option?: any) {
-    option = {
-      chart: { type: 'column' },
-      title: { text: 'Primes Année ' + annee },
-      xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        crosshair: true
-      },
-      yAxis: { min: 0, title: { text: 'Rainfall (mm)' } },
-      tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-          '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-      },
-      plotOptions: {
-        column: {
-          pointPadding: 0.2,
-          borderWidth: 0
-        }
-      },
-      series: [{
-        name: 'Tokyo',
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-
-      }]
-    }
-  }
-
-}
-
 
 
 @Component({
@@ -52,15 +17,21 @@ export class PrimesComponent implements OnInit {
   collegue: Collegue
   listMission: Mission[]
   listAnnee: string[]
-  anneeSelect: string
+  anneeSelect: string = ''
   erreurTechnique = false
 
   constructor(private missionService: MissionService,
-    private authService: AuthService) { }
+    private authService: AuthService) {
+
+  }
 
 
   getAnnee(date: Date): string {
     return date.toString().split("-")[0]
+  }
+
+  getMois(date: Date): string {
+    return date.toString().split("-")[1]
   }
 
   listerAnne() {
@@ -74,6 +45,50 @@ export class PrimesComponent implements OnInit {
 
 
 
+  options: any = {
+    chart: { type: 'column' },
+    title: { text: 'Primes Année ' },
+
+    xAxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      crosshair: true
+    },
+    yAxis: { min: 0, title: { text: '' } },
+    tooltip: {
+      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+      pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+        '<td style="padding:0"><b>{point.y:.1f} €</b></td></tr>',
+      footerFormat: '</table>',
+      shared: true,
+      useHTML: true
+    },
+    plotOptions: {
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0
+      }
+    },
+    series: [{
+      name: '',
+      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    }]
+  }
+
+  getPrimesParMois(): number[] {
+    let data: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    for (let mission of this.listMission) {
+      for (let i = 0; i <= 12; i++)
+        if (parseInt(this.getMois(mission.dateDebut)) == i) {
+          data[i - 1] = data[i - 1] + mission.prime
+        }
+    }
+    this.options.series[0].data = data
+    return data
+  }
+
+
 
   alert(annee: string) {
     this.authService.collegueConnecteObs.subscribe(
@@ -83,26 +98,29 @@ export class PrimesComponent implements OnInit {
           listM => {
             this.listMission = listM.filter(mission => new Date(mission.dateFin) < new Date());
             this.listerAnne();
-            this.listMission = this.listMission.filter(mission => new Date(mission.dateFin) >= new Date(parseInt(annee), 0, 1) && new Date(mission.dateFin) <= new Date(parseInt(annee), 11, 31))
+            this.listMission = this.listMission.filter(mission => new Date(mission.dateFin) >= new Date(parseInt(annee), 0, 1) && new Date(mission.dateFin) <= new Date(parseInt(annee), 11, 31));
+            Highcharts.chart('container', this.options);
           },
         );
       })
+
   }
-  fileName= 'ExcelSheet.xlsx'
 
-  exportexcel(): void 
-  {
-     /* table id is passed over here */   
-     let element = document.getElementById('excel-table'); 
-     const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+  fileName = 'ExcelSheet.xlsx'
 
-     /* generate workbook and add the worksheet */
-     const wb: XLSX.WorkBook = XLSX.utils.book_new();
-     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+  exportexcel(): void {
+    this.fileName = `missions-échues${this.anneeSelect}.xlsx`
+    /* table id is passed over here */
+    let element = document.getElementById('excel-table');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
 
-     /* save to file */
-     XLSX.writeFile(wb, this.fileName);
-    
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
+
   }
 
   ngOnInit(): void {
@@ -119,8 +137,7 @@ export class PrimesComponent implements OnInit {
         );
       })
 
-
-
+    Highcharts.chart('container', this.options);
 
 
   }
